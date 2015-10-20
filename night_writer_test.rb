02921dcdd310a86cecc92394239b52ef1c40eace
Hector Huertas <hectorhuertas@gmail.com>
2015-10-20 13:52:44 -0600

    Describe first tests

---
+++ night_writer_test.rb
@@ -0,0 +1,33 @@
+require 'minitest'
+require 'night_writer'
+
+class NightWriterTest < Minitest::Test
+  def test_class_exists
+    skip
+  end
+
+  def test_class_can_be_instantiated
+    skip
+  end
+
+  def test_it_has_a_file_reader
+    skip
+  end
+
+  def test_it_reads_plain_text
+    skip
+  end
+
+  def test_it_has_a_file_writer
+    skip
+  end
+
+  def test_it_writes_braille_text
+    skip
+  end
+
+  def test_it_encodes_to_braille
+    skip
+  end
+
+end
